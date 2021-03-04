cd5893b4e09cb6e129fa573b41218de8d2c18a9a
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-03-04 17:04:08 -0600

    created  create, update, delete endpoints

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/', (req, res, next) => {
-
+    res.send('Hello World')
 });
+app.post('/create-event', (req, res, next) => {
+    
+})
+
+app.put('/update-event', (req, res, next) => {
+    
+})
+
+app.delete('/delete-event', (req, res, next) => {
+    
+})
+
 
 // Listening port
 app.listen(3000, () => {
