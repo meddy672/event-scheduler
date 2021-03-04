7dfeebfe1c3d90fe24841ea8f01a0426cc14e95e
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-03-04 17:21:32 -0600

    installed and setup body-parser

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+const events = [];
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 // Routes
 app.use('/', (req, res, next) => {
-    res.send('Hello World')
+    res.send('Hello World');
 });
+
 app.post('/create-event', (req, res, next) => {
     
 })
@@ -23,8 +27,8 @@ app.delete('/delete-event', (req, res, next) => {
     
 })
 
-
+const port = 3000;
 // Listening port
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log(`Server is running on port ${port}`)
 });
