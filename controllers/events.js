99476a3cad92aa525c4af613644c17bae958cd8a
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-03-04 20:03:49 -0600

    added logic to handle create event

--- controllers/events.js
+++ controllers/events.js
@@ -1,9 +1,30 @@
-
+const { validationResult } = require('express-validator');
+const moment = require('moment');
 const events = [];
 
 exports.createEvent = (req, res, next) => {
-    console.log(req.body.name)
-    res.send('Response received')
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.error(errors);
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const event = {
+        name: req.body.name,
+        start_time: moment(req.body.start_time),
+        end_time: req.body.end_time,
+        description: req.body.description,
+        location: req.body.location,
+        type: req.body.type
+    };
+    events.push(event);
+    console.log(events);
+    res.status(200).json({
+        message: 'Post created successfully',
+        event: event
+    })
+
 }
 
 exports.updateEvent = (req, res, next) => {
--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const { body } = require('express-validator');
+const { isDate } = require('moment');
 const eventCtrl = require('../controllers/events');
 
 const router = express.Router()
 
 router.post('/',
     [
-        body('name')
-            .trim()
-            .isLength({ min: 5 })
+        body('name').trim().not().isEmpty(),
+        body('start_time').trim().not().isEmpty(),
+        body('end_time').trim().not().isEmpty(),
+        body('description').trim().not().isEmpty(),
+        body('location').trim().not().isEmpty(), 
+        body('type').trim().not().isEmpty(), 
     ],
     eventCtrl.createEvent
 );
